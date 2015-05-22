e12084b18c6a8bcb9c406fa8e8b94a75fce4ca16
Ed Kelley <ekelleyv@gmail.com>
2015-05-22 11:01:07 -0400

    Basic proto working

--- seatjack/server.py
+++ seatjack/server.py
@@ -1,7 +1,12 @@
-import json
-import qrcode
+# -*- coding: utf-8 -*-
 
+import json
+from StringIO import StringIO
+
+import qrcode
 from flask import Flask
+from flask import render_template, send_file
+import bleach
 
 from seatjack.db import Session
 from seatjack.tables import ImageTweet
@@ -9,14 +14,9 @@ from seatjack.tables import ImageTweet
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "TEST"
-
-@app.route('/tickets')
-def tickets():
+def _get_tickets():
     session = Session()
-    image_tweets = session.execute("select min(id), min(tweet_id) as tweet_id, min(tweet_body) as tweet_body, original_image_url from image_tweets where barcode_value IS NOT NULL group by original_image_url")
+    image_tweets = session.execute("select min(id), min(tweet_id) as tweet_id, min(tweet_body) as tweet_body, original_image_url from image_tweets where barcode_value IS NOT NULL group by original_image_url ORDER BY created_at DESC")
 
     ticket_output = []
 
@@ -24,24 +24,59 @@ def tickets():
         ticket = {
             "id" : tweet[0],
             "tweet_id" : tweet[1],
-            "tweet_body" : tweet[2],
+            "tweet_body" : bleach.linkify(tweet[2].decode('utf-8')),
             "image_url" : tweet[3]
         }
         ticket_output.append(ticket)
+    return ticket_output
+
+def _get_ticket(ticket_id):
+    session = Session()
+    tweet = session.query(ImageTweet).filter(ImageTweet.id == ticket_id).first()
+
+    ticket = {
+        "id" : tweet.id,
+        "tweet_id" : tweet.tweet_id,
+        "tweet_body" : bleach.linkify(tweet.tweet_body.decode('utf-8')),
+        "image_url" : tweet.original_image_url,
+        "barcode_value" : tweet.barcode_value
+    }
+    return ticket
+
+def _make_qrcode(value):
+    b = qrcode.make(value, border=2)
+    return b
+
+def serve_pil_image(pil_img):
+    img_io = StringIO()
+    pil_img.save(img_io, 'png')
+    img_io.seek(0)
+    return send_file(img_io, mimetype='image/png')
+
+@app.route('/')
+def index():
+    return render_template('tickets.html', tickets=_get_tickets())
+
+@app.route('/tickets')
+def tickets():
+    ticket_output = _get_tickets()
 
     return json.dumps(ticket_output)
 
-@app.route('/image_view/<int:id>')
-def image_view(id):
-    return "Image view"
+@app.route('/summary/<int:ticket_id>')
+def summary(ticket_id):
+    return render_template('summary.html', ticket=_get_ticket(ticket_id))
 
-@app.route('/barcode_view/<int:id>')
-def barcode_view(id):
-    return "Barcode view"
+@app.route('/ticket/<int:ticket_id>')
+def barcode_view(ticket_id):
+    return render_template('ticket.html', ticket=_get_ticket(ticket_id))
 
-@app.route('/barcode/<int:id>')
-def barcode(id):
-    return "Barcode"
+@app.route('/barcode/<int:ticket_id>')
+def barcode(ticket_id):
+    ticket = _get_ticket(ticket_id)
+    barcode = _make_qrcode(ticket["barcode_value"])
+    return serve_pil_image(barcode)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=1123, debug=True)
